2c100c719f8a5bd34e55e0da4cc39a3cb24820b0
GorDenFreeMan <152312676+GorDemFreeMan@users.noreply.github.com>
2026-01-07 01:52:27 +0300

    Update express.config.ts

--- src/express.config.ts
+++ src/express.config.ts
@@ -1,5 +1,4 @@
-import { miscRouter } from '@/routes';
-import playwrightRouter from '@/routes/index';
+import playwrightRouter from './routes/index';
 import compression from 'compression';
 import 'dotenv/config';
 import express, { type Application } from 'express';
@@ -7,22 +6,17 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 
 const ExpressConfig = (): Application => {
-    const app = express();
-
-    app.use(compression());
-    app.use(
-        express.urlencoded({
-            extended: true,
-        }),
-    );
-    app.use(express.json());
-
-    app.use(helmet());
-    app.use(morgan('dev'));
-
-    app.use('/misc', miscRouter);
-    app.use(playwrightRouter);  // Монтируем на корневой путь
-
-    return app;
+  const app = express();
+  
+  app.use(compression());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(helmet());
+  app.use(morgan('dev'));
+  
+  app.use(playwrightRouter);
+  
+  return app;
 };
+
 export default ExpressConfig;
